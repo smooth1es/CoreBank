2659949f32db8082a28d78ac28275ad1cd43f4e2
smooth1es <vladik1403083@gmail.com>
2026-06-10 14:41:01 +0200

    feat: add abstract Account class with debit and credit logic

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.math.BigDecimal;
+
+public abstract class Account {
+    private final String id;
+    private final String userId;
+    private BigDecimal balance;
+
+    public Account(String id, String userId, BigDecimal balance) {
+        this.id = id;
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public abstract void withdraw(BigDecimal amount);
+
+    public void deposit (BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+}
